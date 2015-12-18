5f828f19792d57471e17c1a531ad249bed203252
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 00:48:24 +0900

    Digit confirms to Foldable

--- FingerTree/Digit.swift
+++ FingerTree/Digit.swift
@@ -6,3 +6,21 @@ public enum Digit<T> {
 	case Three(T, T, T)
 	case Four(T, T, T, T)
 }
+
+extension Digit where T: Monoid {
+	func foldMap<U: Monoid>(f: T -> U) -> U {
+		switch self {
+		case let .One(a):
+			return f(a)
+			
+		case let .Two(a, b):
+			return f(a).mappend(f(b))
+			
+		case let .Three(a, b, c):
+			return f(a).mappend(f(b).mappend(f(c)))
+			
+		case let .Four(a, b, c, d):
+			return f(a).mappend(f(b).mappend(f(c).mappend(f(d))))
+		}
+	}
+}
