6954cd2227d5c48974b2276d6f042980e8938802
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 00:59:27 +0900

    Add nodeToDigit

--- FingerTree/Node.swift
+++ FingerTree/Node.swift
@@ -16,3 +16,13 @@ extension Node {
 		}
 	}
 }
+
+public func nodeToDigit<V, A>(node: Node<V, A>) -> Digit<A> {
+	switch node {
+	case let .Node2(_, a, b):
+		return .Two(a, b)
+		
+	case let .Node3(_, a, b, c):
+		return .Three(a, b, c)
+	}
+}
