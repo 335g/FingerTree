b39f2b23dc0a3ea42da7ff91a1e267f768c73378
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 00:54:27 +0900

    Node confirms to Foldable

--- FingerTree/Node.swift
+++ FingerTree/Node.swift
@@ -4,3 +4,15 @@ public enum Node<T, U> {
 	case Node2(T, U, U)
 	case Node3(T, U, U, U)
 }
+
+extension Node {
+	public func foldMap<V: Monoid>(f: U -> V) -> V {
+		switch self {
+		case let .Node2(_, a, b):
+			return f(a).mappend(f(b))
+			
+		case let .Node3(_, a, b, c):
+			return f(a).mappend(f(b).mappend(f(c)))
+		}
+	}
+}
