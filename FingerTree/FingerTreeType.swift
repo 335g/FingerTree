558e4062a3f089a9609c0ed7a22c1ac55727e1e3
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-18 21:20:56 +0900

    Move to new file

---
+++ FingerTree/FingerTreeType.swift
@@ -0,0 +1,9 @@
+//
+//  FingerTreeType.swift
+//  FingerTree
+//
+//  Created by Yoshiki Kudo on 2015/12/18.
+//  Copyright © 2015年 Yoshiki Kudo. All rights reserved.
+//
+
+import Foundation
---
+++ FingerTree/Monoid.swift
@@ -0,0 +1,22 @@
+//  Copyright © 2015 Yoshiki Kudo. All rights reserved.
+
+protocol Semigroup {
+	func mappend(other: Self) -> Self
+}
+
+infix operator <> {
+associativity right
+precedence 160
+}
+
+func <> <S: Semigroup>(lhs: S, rhs: S) -> S {
+	return lhs.mappend(rhs)
+}
+
+func sconcat <S: Semigroup>(initial: S, t: [S]) -> S {
+	return t.reduce(initial){ $0 <> $1 }
+}
+
+protocol Monoid: Semigroup {
+	static var mempty: Self { get }
+}
