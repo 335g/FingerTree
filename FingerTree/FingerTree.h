4cee2c6e03fa85460ce33543f9ea7ce33e0854eb
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-18 20:42:27 +0900

    first commit

---
+++ FingerTree/FingerTree.h
@@ -0,0 +1,19 @@
+//
+//  FingerTree.h
+//  FingerTree
+//
+//  Created by Yoshiki Kudo on 2015/12/18.
+//  Copyright © 2015年 Yoshiki Kudo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FingerTree.
+FOUNDATION_EXPORT double FingerTreeVersionNumber;
+
+//! Project version string for FingerTree.
+FOUNDATION_EXPORT const unsigned char FingerTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FingerTree/PublicHeader.h>
+
+
