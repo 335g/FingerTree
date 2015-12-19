c6cc5e99c5027bfc406fbb520283847b4d4bce98
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 20:11:14 +0900

    Implement appendTree2

--- FingerTree/Concatenation.swift
+++ FingerTree/Concatenation.swift
@@ -61,7 +61,34 @@ public func appendTree1<V, A>(tree: FingerTree<V, A>) -> A -> FingerTree<V, A> -
 }
 
 public func appendTree2<V, A>(tree: FingerTree<V, A>) -> A -> A -> FingerTree<V, A> -> FingerTree<V, A> {
-	fatalError()
+	return { a in
+		{ b in
+			{ tree1 in
+				switch (tree, a, b, tree1) {
+				case (.Empty, _, _, _):
+					return a <| b <| tree1
+					
+				case (_, _, _, .Empty):
+					return tree |> a |> b
+					
+				case let (.Single(x), _, _, _):
+					return x <| a <| b <| tree1
+					
+				case let (_, _, _, .Single(x)):
+					return tree |> a |> b |> x
+					
+				case let (.Deep(_, pr1, m1, sf1), _, _, .Deep(_, pr2, m2, sf2)):
+					return FingerTree<V, A>.deep(
+						prefix: pr1,
+						deeper: addDigits2(m1)(sf1)(a)(b)(pr2)(m2),
+						suffix: sf2)
+					
+				default:
+					fatalError()
+				}
+			}
+		}
+	}
 }
 
 public func appendTree3<V, A>(tree: FingerTree<V, A>) -> A -> A -> A -> FingerTree<V, A> -> FingerTree<V, A> {
