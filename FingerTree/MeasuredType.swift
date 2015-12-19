a84933dafcc3f18f9a03fca5d3c85547e3bf1ec8
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 10:40:14 +0900

    Delete MeasuredType

--- FingerTree/MeasuredType.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2015 Yoshiki Kudo. All rights reserved.
-
-public protocol MeasuredType {
-	typealias Value: Monoid
-	typealias Annotation
-	
-	func measure(annotation: Annotation) -> Value
-}
-
