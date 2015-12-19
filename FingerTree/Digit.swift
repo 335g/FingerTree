f4b6df6d8b9cf12f21e01caae28e8001d4c86909
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 22:22:35 +0900

    Implement splitDigit

--- FingerTree/Digit.swift
+++ FingerTree/Digit.swift
@@ -23,6 +23,54 @@ public enum Digit<A: MeasuredType> {
 			return .Four(f(a), f(b), f(c), f(d))
 		}
 	}
+	
+	// MARK: - split
+	
+	public func splitDigit<V: Monoid, A: MeasuredType where V == A.MeasuredValue>(predicate: V -> Bool) -> V -> Digit<A> -> Split<Digit<A>?, A> {
+		return { i in
+			{ digit in
+				switch digit {
+				case let .One(a):
+					return .Split(nil, a, nil)
+
+				case let .Two(a, b):
+					let va = i.mappend(a.measure())
+					if predicate(va) {
+						return .Split(nil, a, .One(b))
+					}else {
+						return .Split(.One(a), b, nil)
+					}
+					
+				case let .Three(a, b, c):
+					let va = i.mappend(a.measure())
+					let vab = va.mappend(b.measure())
+					
+					if predicate(va) {
+						return .Split(nil, a, .Two(b, c))
+					}else if predicate(vab) {
+						return .Split(.One(a), b, .One(c))
+					}else {
+						return .Split(.Two(a, b), c, nil)
+					}
+					
+				case let .Four(a, b, c, d):
+					let va = i.mappend(a.measure())
+					let vab = va.mappend(b.measure())
+					let vabc = vab.mappend(c.measure())
+					
+					if predicate(va) {
+						return .Split(nil, a, .Three(b, c, d))
+					}else if predicate(vab) {
+						return .Split(.One(a), b, .Two(c, d))
+					}else if predicate(vabc) {
+						return .Split(.Two(a, b), c, .One(d))
+					}else {
+						return .Split(.Three(a, b, c), d, nil)
+					}
+				}
+			}
+		}
+	}
 }
 
 // MARK: - Foldable
