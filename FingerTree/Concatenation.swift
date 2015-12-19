2c8eba14a7e94b926229f09b008022ec4659b94e
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-19 19:34:56 +0900

    Prepare appendTreeX/addDigitsX

---
+++ FingerTree/Concatenation.swift
@@ -0,0 +1,45 @@
+//  Copyright © 2015 Yoshiki Kudo. All rights reserved.
+
+// MARK: - appendTree X
+
+public func appendTree0<V, A>(tree: FingerTree<V, A>) -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func appendTree1<V, A>(tree: FingerTree<V, A>) -> A -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func appendTree2<V, A>(tree: FingerTree<V, A>) -> A -> A -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func appendTree3<V, A>(tree: FingerTree<V, A>) -> A -> A -> A -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func appendTree4<V, A>(tree: FingerTree<V, A>) -> A -> A -> A -> A -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+// MARK: - addDigits X
+
+public func addDigits0<V, A>(tree: FingerTree<V, A>) -> Digit<A> -> Digit<A> -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func addDigits1<V, A>(tree: FingerTree<V, A>) -> Digit<A> -> A -> Digit<A> -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func addDigits2<V, A>(tree: FingerTree<V, A>) -> Digit<A> -> A -> A -> Digit<A> -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func addDigits3<V, A>(tree: FingerTree<V, A>) -> Digit<A> -> A -> A -> A -> Digit<A> -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
+
+public func addDigits4<V, A>(tree: FingerTree<V, A>) -> Digit<A> -> A -> A -> A -> A -> Digit<A> -> FingerTree<V, A> -> FingerTree<V, A> {
+	fatalError()
+}
