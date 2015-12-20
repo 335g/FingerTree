435d39de32779a6103d14db024e839913bb0e81b
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-20 09:04:28 +0900

    Change static function to global function

--- FingerTree/Node.swift
+++ FingerTree/Node.swift
@@ -5,25 +5,15 @@ public enum Node<V: Monoid, A: MeasuredType where V == A.MeasuredValue> {
 	case Node2(V, A, A)
 	case Node3(V, A, A, A)
 	
-	// MARK: - static
-	
-	public static func node2(a: A, _ b: A) -> Node<V, A> {
-		return .Node2(a.measure().mappend(b.measure()), a, b)
-	}
-	
-	public static func node3(a: A, _ b: A, _ c: A) -> Node<V, A> {
-		return .Node3(a.measure().mappend(b.measure().mappend(c.measure())), a, b, c)
-	}
-	
 	// MARK: - map
 	
 	public func map<V1: Monoid, A1: MeasuredType where V1 == A1.MeasuredValue>(f: A -> A1) -> Node<V1, A1> {
 		switch self {
 		case let .Node2(_, a, b):
-			return Node<V1, A1>.node2(f(a), f(b))
+			return node2(f(a), f(b))
 			
 		case let .Node3(_, a, b, c):
-			return Node<V1, A1>.node3(f(a), f(b), f(c))
+			return node3(f(a), f(b), f(c))
 		}
 	}
 }
@@ -56,4 +46,14 @@ extension Node: MeasuredType {
 			return v
 		}
 	}
+}
+
+// MARK: - Other
+
+public func node2<V: Monoid, A: MeasuredType where V == A.MeasuredValue>(a: A, _ b: A) -> Node<V, A> {
+	return .Node2(a.measure().mappend(b.measure()), a, b)
+}
+
+public func node3<V: Monoid, A: MeasuredType where V == A.MeasuredValue>(a: A, _ b: A, _ c: A) -> Node<V, A> {
+	return .Node3(a.measure().mappend(b.measure().mappend(c.measure())), a, b, c)
 }
