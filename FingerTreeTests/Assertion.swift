3865bc2cf05ec56a687fed696a004f911571005b
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-23 19:46:21 +0900

    Needs to Assertion

---
+++ FingerTreeTests/Assertion.swift
@@ -0,0 +1,6 @@
+//  Copyright © 2015 Yoshiki Kudo. All rights reserved.
+
+import XCTest
+import Assertions
+
+
--- FingerTreeTests/Fixtures.swift
+++ FingerTreeTests/Fixtures.swift
@@ -3,57 +3,19 @@
 import Foundation
 @testable import FingerTree
 
-enum Entry<V: Comparable, A> {
-	case Ent(V, A)
-	
-	init(_ v: V, _ a: A){
-		self = .Ent(v, a)
+public typealias Size = UInt
+
+extension Size: Monoid {
+	public static var mempty: Size { return 0 }
+	public func mappend(other: Size) -> Size {
+		return self + other
 	}
 }
 
-enum Priority<V: Comparable, A> {
-	case NoPrio
-	case Prio(V, A)
-}
-
-extension Priority: Monoid {
-	static var mempty: Priority { return .NoPrio }
-	func mappend(other: Priority) -> Priority {
-		switch (self, other) {
-		case let (x, .NoPrio):
-			return x
-		case let (.NoPrio, x):
-			return x
-		case let (.Prio(kx, _), .Prio(ky, _)):
-			if kx <= ky {
-				return self
-			}else {
-				return other
-			}
-		default:
-			fatalError()
-		}
-	}
-}
-
-extension Entry: Measurable {
-	typealias MeasuredValue = Priority<V, A>
-	func measure() -> MeasuredValue {
-		switch self {
-		case let .Ent(v, a):
-			return .Prio(v, a)
-		}
-	}
-}
-
-func == <V, A: Equatable>(lhs: Priority<V, A>, rhs: Priority<V, A>) -> Bool {
-	switch (lhs, rhs) {
-	case (.NoPrio, .NoPrio):
-		return true
-	case let (.Prio(v1, a1), .Prio(v2, a2)):
-		return v1 == v2 && a1 == a2
-	default:
-		return false
+extension Character: Measurable {
+	public typealias MeasuredValue = Size
+	public func measure() -> MeasuredValue {
+		return 1
 	}
 }
 
