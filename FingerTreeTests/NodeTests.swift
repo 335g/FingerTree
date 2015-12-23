e9c95bb2693d03cbab03480ed9f0ba1d79d5f1f4
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-23 13:57:24 +0900

    Remove parts of no relationship

--- FingerTreeTests/NodeTests.swift
+++ FingerTreeTests/NodeTests.swift
@@ -6,27 +6,6 @@ import Assertions
 
 final class NodeTests: XCTestCase {
 	
-	func testNodeSatisfyLawOfSemigroup(){
-		// (x + y) + z == x + (y + z)
-		
-		let entry11 = Entry<Int, String>(1, "a")
-		let entry12 = Entry<Int, String>(2, "b")
-		let node1 = Node.node2(entry11, entry12)
-		
-		let entry21 = Entry<Int, String>(3, "c")
-		let entry22 = Entry<Int, String>(4, "d")
-		let node2 = Node.node2(entry21, entry22)
-		
-		let entry31 = Entry<Int, String>(5, "e")
-		let entry32 = Entry<Int, String>(6, "f")
-		let node3 = Node.node2(entry31, entry32)
-		
-		assertEqual(
-			(node1.measure() <> node2.measure()) <> node3.measure(),
-			node1.measure() <> (node2.measure() <> node3.measure())
-		)
-	}
-	
 	func testStaticNode2ProvidesNodeWithMeasuredValue(){
 		let entry1 = Entry<Int, String>(10, "a")
 		let entry2 = Entry<Int, String>(20, "b")
